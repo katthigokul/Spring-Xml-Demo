acc4cd8dbc8916b5ceb9bb4a938e15c477a1776b
katthigokul <gokulkatthi@gmail.com>
2019-07-27 14:16:40 +0530

    created Task3

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -27,26 +27,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        Movie movieFirst = (Movie) xmlBeanFactory.getBean("movie1");
-        Movie moviesecond = (Movie) xmlBeanFactory.getBean("movie1");
-
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
         Movie movie = (Movie) applicationContext.getBean("movie2");
-        Movie movie2 = (Movie) applicationContext.getBean("movie2");
-
-        Movie movieB = (Movie) applicationContext.getBean("movieB");
-
-
-        System.out.println(movieFirst.getActor());
+        Movie movie2 = (Movie) applicationContext.getBean("movie3");
 
         System.out.println(movie.getActor());
-
-        System.out.println(movie == movie2);
-
-        System.out.println(movieFirst == moviesecond);
-
-        System.out.println(movieB.getActor());
-
+        System.out.println(movie2.getActor());
     }
 }
--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -44,4 +44,5 @@ public class Actor {
         this.gender = gender;
         this.age = age;
     }
+
 }
